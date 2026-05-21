0c3bddeecae1edbf40588fb89c63213cec14ea86
Samuel Ashiuloka <noblesam3805@gmail.com>
2026-05-21 16:31:06 +0100

    first commit

---
+++ practice.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class practice{
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+
+        int day= input.nextInt();
+        
+            if(day==1){
+            System.out.println("day is: Sunday");
+            }
+            else if(day==2){
+            System.out.println("day is: Monday");
+            }
+             else if(day==3){
+            System.out.println("day is: Tuesday");
+            }
+            else if(day==4){
+            System.out.println("day is: Wednesday");
+            }
+            else if(day==5){
+            System.out.println("day is: Thursday");
+            }
+            else if(day==6){
+            System.out.println("day is: Friday");
+            }
+            else if(day==7){
+            System.out.println("day is: Saturday");
+            }
+            else{
+                System.out.println("Invalid input");
+            }
+        
+    }
+}
