9f08f91c979bb57cf39ca46363c9c5703c7c53aa
Samuel Ashiuloka <noblesam3805@gmail.com>
2026-05-25 15:05:05 +0100

    chapter 3

---
+++ Chapter 3/TwoLargestNumbers.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class TwoLargestNumbers {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.print("Enter number " + i + ": ");
+            int number = input.nextInt();
+
+            if (number > largest) {
+                secondLargest = largest;
+                largest = number;
+            }
+
+            else if (number > secondLargest) {
+                secondLargest = number;
+            }
+        }
+
+        System.out.println("Largest number: " + largest);
+        System.out.println("Second largest number: " + secondLargest);
+
+        input.close();
+    }
+}
